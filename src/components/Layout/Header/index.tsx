9e105a638abc4e3e2b4f10c4f9ac1a47b596f7f1
Elk Leung <elkleung@gmail.com>
2022-04-20 18:00:20 +0800

    fix

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="d-flex justify-content-between align-items-center" id={styles.header}>
       <div className={styles.logo}><Link href="/"><a className="d-block"></a></Link></div>
-      <div className={`${styles.buttons} buttons`}><a className={`${styles.btn} btn`} href="https://discord.gg/rbsfr4KMR7" target="_blank" rel="noreferrer">Join Discord</a></div>
+      <div className={`${styles.buttons} buttons`}><a className={`${styles.btn} btn`} href="https://discord.gg/thespace" target="_blank" rel="noreferrer">Join Discord</a></div>
     </header>
   );
 };
