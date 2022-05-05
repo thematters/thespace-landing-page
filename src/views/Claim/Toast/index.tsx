a706b8e5c0b4ed48613c04919b858523b4a4f513
robertu <4065233+robertu7@users.noreply.github.com>
2022-05-05 18:55:31 +0800

    feat: formatting

--- src/views/Claim/Toast/index.tsx
+++ src/views/Claim/Toast/index.tsx
@@ -3,33 +3,34 @@ import classNames from "classnames";
 import styles from "./styles.module.sass";
 
 export interface AirdropProps {
-  status?: 'success' | 'failed'
+  status?: "success" | "failed";
 }
 
 const Toast = ({ status }: AirdropProps) => {
-
   const toastClasses = classNames({
     [styles.toast]: true,
     [styles[`${status}`]]: status,
-    'd-flex': true,
-    'justify-content-center': true,
-    'align-items-center': true
+    "d-flex": true,
+    "justify-content-center": true,
+    "align-items-center": true,
   });
 
   return (
     <div className={toastClasses}>
-      {status === 'success' &&
+      {status === "success" && (
         <>
           <span>Claim successfully 🎉</span>
           <span className={styles.icon}></span>
         </>
-      }
-      {status === 'failed' &&
+      )}
+      {status === "failed" && (
         <>
-          <span>Claim failed. Please check wallet settings or try it later.</span>
+          <span>
+            Claim failed. Please check wallet settings or try it later.
+          </span>
           <span className={styles.icon}></span>
         </>
-      }
+      )}
     </div>
   );
 };
