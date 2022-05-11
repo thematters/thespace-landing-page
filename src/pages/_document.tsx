fa7e6136267dbdd48f6524c8c3c0cff2e6d35537
49659410+tx0c <>
2022-05-11 18:20:37 +0000

    feat(google-analytics): with process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,20 @@ class TheSpaceDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+            <>
+              {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date); gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname});`,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
