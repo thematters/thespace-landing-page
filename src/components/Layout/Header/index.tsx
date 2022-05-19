0d47aa1e2ce5007e075a6bc191032d2b3ee5dc0c
Elk Leung <elkleung@gmail.com>
2022-05-19 18:57:21 +0800

    only countdown to production

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,8 +1,44 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
+import classNames from "classnames";
+
+import { useCountdown } from "~/hooks";
 
 import styles from "./styles.module.sass";
 
 const Header = () => {
+  const [isActive, setActive] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+  const [isCountdown, setCountdown] = useState(false);
+
+  const activeClasses = classNames({
+    [styles.active]: isActive,
+  });
+
+  const toggleActive = () => {
+    setActive(!isActive);
+  };
+
+  const router = useRouter();
+  useEffect(() => {
+    if (isActive) {
+      setActive(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.asPath]);
+
+  useEffect(() => {
+    if (new Date() < new Date(process.env.NEXT_PUBLIC_CLAIM_START_AT || "")) {
+      setCountdown(true);
+    }
+  }, []);
+
+  const { days, hours, minutes, seconds } = useCountdown({
+    end: process.env.NEXT_PUBLIC_CLAIM_START_AT || "",
+    onEnd: () => setIsStarted(true),
+  });
+
   return (
     <header
       className="d-flex justify-content-between align-items-center"
@@ -13,16 +49,51 @@ const Header = () => {
           <a className="d-block"></a>
         </Link>
       </div>
-      <div className={`${styles.buttons} buttons`}>
+      <div className={`${styles.menu} ${activeClasses}`}>
         <a
-          className={`${styles.btn} btn`}
           href="https://discord.gg/thespace"
           target="_blank"
           rel="noreferrer"
+          className={`${styles.strong}`}
         >
           Join Discord
         </a>
       </div>
+      <div className={`${styles.burger} ${activeClasses} d-lg-none`}>
+        <button onClick={toggleActive}></button>
+      </div>
+      {isCountdown && !isStarted && (
+        <div
+          className={`${styles.countdown} d-flex justify-content-between align-items-center`}
+        >
+          <div className={`${styles.time}`}>
+            <div className="d-flex justify-content-between align-items-center">
+              <i className={styles.icon}></i>
+              <span>
+                <b className="d-none d-lg-inline-block">
+                  The Space Official Launch in
+                </b>{" "}
+                <b>{days}</b> days <b>{hours}</b> hours <b>{minutes}</b> mins{" "}
+                <b>{seconds}</b> sec
+              </span>
+            </div>
+          </div>
+          <div className={`${styles.buttons}`}>
+            <a
+              className="d-flex justify-content-between align-items-center"
+              href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20220531T120000Z%2F20220531T120000Z&text=The%20Space%20Official%20Launch"
+              title="Save Event in my Calendar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="d-none d-lg-inline-block">
+                <b>Add to Calendar</b>
+              </span>
+              <i className={styles.icon}></i>
+            </a>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
