fdba60a13efaaab0a3ff293fecdd434213601c99
robertu <4065233+robertu7@users.noreply.github.com>
2022-06-09 09:46:05 +0800

    fix: rename FAIRDROP_CONTRACT to NEXT_PUBLIC_FAIRDROP_CONTRACT

--- src/views/Claim/Fairdrop/index.tsx
+++ src/views/Claim/Fairdrop/index.tsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Steps from "./Steps";
 import Results from "./Results";
+import { useAccount, useNetwork } from "wagmi";
 
 export interface FairdropProps {
   back: () => void;
 }
 
 const Fairdrop: React.FC<FairdropProps> = ({ back }) => {
+  const { data: accountData } = useAccount();
+  const { activeChain } = useNetwork();
+  const account = accountData?.address;
+  const isUnsupportedNetwork = activeChain?.unsupported;
+
   const [resultStatus, setResultStatus] = useState("");
+
+  useEffect(() => {
+    if (!account || isUnsupportedNetwork) {
+      back();
+      return;
+    }
+  }, [account, isUnsupportedNetwork]);
+
   return (
     <>
       {resultStatus !== "" ? (
